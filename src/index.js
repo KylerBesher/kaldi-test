b8cfbad4256f3b4e989efabf56bb6798802b6b50
Kyler Besher <kylerbesher@Kylers-MacBook-Pro.local>
2022-02-09 23:32:58 -0600

    update

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import "./css/main.css";
 // Say hello
 console.log("🦊 Hello! Edit me in src/index.js");
 
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 $(document).ready(() => {
 
 
